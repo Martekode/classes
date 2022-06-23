bac2d12d704add6fde775092cf1f99143d09590a
Marteko9000 <themarteko@protonmail.com>
2022-06-23 15:58:32 +0200

    excersise 3 finished

--- exercise_3_private.php
+++ exercise_3_private.php
@@ -16,3 +16,52 @@ TODO: Print this method on the screen on a new line.
 
 USE TYPEHINTING EVERYWHERE!
 */
+class Beverage {
+    private string $color;
+    private float $price;
+    private string $temperature;
+    private string $name;
+
+    public function __construct(string $name,string $color,float $price){
+        $this->temperature = 'cold';
+        $this->name = $name;
+        $this->color = $color;
+        $this->price = $price;
+    }
+    public function getInfo():string{
+        $message = "This beverage is $this->temperature and $this->color for € $this->price.";
+        return $message;
+    }
+    public function getColor():string{
+        return $this->color;
+    }
+    public function setColor(string $color):string{
+        return $this->color = $color;
+    }
+}
+#extention
+class Beer extends Beverage {
+    private float $alcoholPercentage;
+
+    public function __construct(string $name, string $color, float $price,float $alcoholPercentage){
+        $this->alcoholPercentage = $alcoholPercentage;
+        parent:: __construct($name, $color, $price);
+        
+    }
+    public function disAlcoholPercentage():void{
+        echo "</br>".$this->alcoholPercentage;
+    }
+    public function getAlcoholPercentage():float{
+        return $this->alcoholPercentage;
+    }
+    public function beerInfo():string{
+        return "</br> Hi i'm Duvel and have an alcochol percentage of " . $this->getAlcoholPercentage() . " and I have a ". $this->getColor() . " color";
+    }
+
+}
+$duvel = new Beer('duvel','blond',3.5,8.5);
+echo $duvel->getInfo();
+echo "</br>" . $duvel->getAlcoholPercentage();
+$duvel->disAlcoholPercentage();
+echo "</br>".$duvel->setColor('light');
+echo "</br>" . $duvel->beerInfo();
