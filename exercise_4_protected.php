8b27a42d8b5cf3212bd65dd38b41709ebeab1317
Martekode <BrianDesmedt9000@hotmail.com>
2022-06-24 09:10:04 +0200

    not sure if i do something wrong because everything just works ex4

--- exercise_4_protected.php
+++ exercise_4_protected.php
@@ -11,3 +11,43 @@ TODO: Make all the other prints work without error without changing the beverage
 
 USE TYPEHINTING EVERYWHERE!
 */
+class Beverage {
+    protected string $color;
+    protected float $price;
+    protected string $temperature;
+    protected string $name;
+
+    public function __construct(string $name,string $color,float $price){
+        $this->temperature = 'cold';
+        $this->name = $name;
+        $this->color = $color;
+        $this->price = $price;
+    }
+    public function getInfo():string{
+        $message = "This beverage is $this->temperature and $this->color.";
+        return $message;
+    }
+
+}
+
+#extention
+class Beer extends Beverage {
+    protected float $alcoholPercentage;
+
+    public function __construct(string $name, string $color, float $price,float $alcoholPercentage){
+        $this->alcoholPercentage = $alcoholPercentage;
+        parent:: __construct($name, $color, $price);
+        
+    }
+    public function getAlcoholPercentage(){
+        return $this->alcoholPercentage;
+    }
+    public function disAlcoholPercentage():void{
+        echo "</br>".$this->alcoholPercentage;
+    }
+
+}
+$duvel = new Beer('duvel','blond',3.5,8.5);
+echo "</br>" . $duvel->disAlcoholPercentage();
+echo "</br>" . $duvel->getAlcoholPercentage();
+
